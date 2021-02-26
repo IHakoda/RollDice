c50fde06ca118d674ecc61dbb52eee4d335833a7
Andrii <Hakoda.AK@gmail.com>
2021-02-26 08:57:21 +0200

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by mr. Hakoda on 26.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
